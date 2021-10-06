2ff84524b5c2d3d161568ed273ace157343e0176
Ashleigh Schuldt <ashleigh.schuldt@gmail.com>
2021-10-05 21:48:51 -0600

    fixing schema on db setup for arrival times table

--- db/setup/createArrivalTimesTable.sql
+++ db/setup/createArrivalTimesTable.sql
@@ -2,6 +2,6 @@
 create table arrival_times (
 	id serial primary key,
 	train_id int,
-	foreign key (station_id) references stations(id),
+	foreign key (train_id) references trains(id),
 	arrival_time varchar(8)
 	);
