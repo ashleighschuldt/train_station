7ea9519e810d308559298fd4d3b1a5aeb32b5f2b
Ashleigh Schuldt <33530572+ashleighschuldt@users.noreply.github.com>
2021-10-06 07:42:30 -0600

    Update seedArrivalTimes.sql

--- db/setup/seedArrivalTimes.sql
+++ db/setup/seedArrivalTimes.sql
@@ -1,1 +1,1 @@
-insert into arrival_times (station_id, arrival_time) values (1, '22:00:00'), (2, '22:00:00'), (1, '09:00:00'),(2, '10:00:00'),(1, '24:99:00');
+insert into arrival_times (station_id, arrival_time) values (1, '22:00:00'), (2, '22:00:00'), (1, '09:00:00'),(2, '10:00:00'),(1, '23:50:00');
